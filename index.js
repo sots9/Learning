3f79cbbdab589297cdfd17448da498ced27e2bc1
Shrijan <shrijan.2016@gmail.com>
2017-06-08 19:06:04 +0545

    Express basic

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const http = require('http');
+const express = require('express');
+const con = express();
 const port = 3000;
 
-const customRequestHandler = (request, response) => {
+con.get('/', (request, response) => {
   console.log('This is a new url');
-  response.end('Hello! welcome to basic http server');
-}
+  response.send('Hello! welcome to basic http server with express');
+});
 
-const server = http.createServer(customRequestHandler);
-
-server.listen(port, (err) => {
+con.listen(port, (err) => {
   if (err) {
     return console.log('New error');
   }
 
-  console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port} through express`);
 });
 
